3b15db0fe4791b9b321bc7070346a08eecfbc3b0
Xrysnow <xrysnow@outlook.com>
2023-03-30 01:36:15 +0800

    修复bgm功能

--- js/reader.js
+++ js/reader.js
@@ -60,7 +60,6 @@ class ReaderParam {
         this.chTitles = null;
         this.chPages = null;
         this.hiddenPages = null;
-        this.bgmVolume = null;
         this.bgmInfo = null;
         this.bgmExtId = null;
         this.i18nString = null;
@@ -68,6 +67,20 @@ class ReaderParam {
         this.fnGetChapterCoverSrc = null;
         this.fnGetImgSrc = null;
         this.editorNote = null;
+        this.bgmVolume = [
+            -9.86, -7.43, -8.97, -14.48, -9.31,
+            -5.78, -15.47, -1.39, -14.51, -16.47,
+            -8.50, -12.84, -11.49, -7.69, -11.63,
+            0/* 8.25 */, -12.30, -7.80, -14.55, -11.50,
+            -15.37, -14.30, 0, -12.19, -14.65,
+            -14.17, -14.18, -12.11, -14.47, -12.89,
+            //TODO
+            0, 0, 0, 0, 0,
+            0, 0, 0, 0, 0,
+            0, 0, 0, 0, 0,
+            0, 0, 0, 0, 0,
+        ];
+        this.bgmVolume[98] = 0;
     }
 }
 
@@ -90,6 +103,7 @@ const Reader = function (param) {
     let i18nHtml = param.i18nHtml
     let PARAMETER = param
 
+    const DEBUG_SHOW_SCROLL_RATIO = true
     let LOCAL_MODE = false
     let AUDIO_LOCAL_MODE = true
     let VOICE_LANGUAGE = 'zh'
@@ -154,7 +168,7 @@ const Reader = function (param) {
         BGM_INFO = BGM_INFO[LANGUAGE]
     }
 
-    const MUSIC_LOCAL_SRC_PREFIX = 'res/music/'
+    const MUSIC_LOCAL_SRC_PREFIX = '../res/music/'
     const MUSIC_LOCAL_SRC_POSTFIX = '.mp3'
     // const VOICE_SRC_POSTFIX = '.wav'
     // const VOICE_ICON = '<svg class="menu-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 48 48"><path d="M6 18v12h8l10 10V8L14 18H6zm27 6c0-3.53-2.04-6.58-5-8.05v16.11c2.96-1.48 5-4.53 5-8.06zM28 6.46v4.13c5.78 1.72 10 7.07 10 13.41s-4.22 11.69-10 13.41v4.13c8.01-1.82 14-8.97 14-17.54S36.01 8.28 28 6.46z"/></svg>'
@@ -844,8 +858,9 @@ const Reader = function (param) {
                 SetBgMusicHandle(id, BgMusicPlayerHeight, 500)
             }
         }
-        //debug
-        // document.getElementById('chapter-title').textContent = ratio.toFixed(2)
+        if (DEBUG_SHOW_SCROLL_RATIO) {
+            document.getElementById('chapter-title').textContent = ratio.toFixed(2)
+        }
     })
 
     let IsFirstClick = true
