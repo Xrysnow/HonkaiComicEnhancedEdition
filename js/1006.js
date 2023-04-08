99a34ca13430ccc7b29cb87aa71ef83c67cdf520
Xrysnow <xrysnow@outlook.com>
2023-04-08 18:34:18 +0800

    更新编注

--- js/1006.js
+++ js/1006.js
@@ -9,7 +9,8 @@
     }
     p.editorNote = {
         zh: '- 本篇为《圣痕之谜篇》的后续。'
-        + '</br>- <a href="https://www.bilibili.com/video/BV1Wx411S7hm">漫画纪念PV</a>'
+            + '</br>- 第十一话与第十二话之间原为「轩辕剑秘话」，已被移动到《神之键秘话》。'
+            + '</br>- <a href="https://www.bilibili.com/video/BV1Wx411S7hm">漫画纪念PV</a>'
     }
     let prefix = Util.getImgSrcPrefix()
     p.bookCoverSrc = prefix + 'book_cover/' + bookNum + '.jpg'
