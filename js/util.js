e3df9312b160631634bf3cf84328cd95658e3e74
Xrysnow <xrysnow@outlook.com>
2023-06-04 18:15:06 +0800

    修正清除数据代码

--- js/util.js
+++ js/util.js
@@ -68,14 +68,14 @@ const Settings = {
             return
         }
         let keys = Object.keys(this)
-        try {
-            for (let i = 0; i < keys.length; i++) {
-                const k = keys[i]
-                if (k[0] == 'K') {
-                    window.localStorage.removeItem(k)
+        for (let i = 0; i < keys.length; i++) {
+            const k = keys[i]
+            if (k[0] == 'K') {
+                try {
+                    window.localStorage.removeItem(this[k])
+                } catch (error) {
                 }
             }
-        } catch (error) {
         }
     },
 
